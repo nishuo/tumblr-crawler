089e1638a5981fd0fa75c644667f03960e420098
玄魂 <xuanhun521@126.com>
2017-01-18 22:12:24 +0800

    Update tumblr-photo-video-ripper.py
    简单修改，中文注释，去掉冗余方法

--- tumblr-photo-video-ripper.py
+++ tumblr-photo-video-ripper.py
@@ -10,20 +10,26 @@ import re
 import json
 
 
-# Setting timeout
+# 设置请求超时时间
 TIMEOUT = 10
 
-# Retry times
+# 尝试次数
 RETRY = 5
 
-# Medium Index Number that Starts from
+# 分页请求的起始点
 START = 0
 
-# Numbers of photos/videos per page
+# 每页请求个数
 MEDIA_NUM = 50
 
-# Numbers of downloading threads concurrently
+# 并发线程数
 THREADS = 10
+
+# 是否下载图片
+ISDOWNLOADIMG=True
+
+#是否下载视频
+ISDOWNLOADVIDEO=True
 
 
 class DownloadWorker(Thread):
@@ -68,10 +74,10 @@ class DownloadWorker(Thread):
                     except IndexError:
                         return None
         except:
-            raise TypeError("Unable to find the right url for downloading. "
-                            "Please open a new issue on "
-                            "https://github.com/dixudx/tumblr-crawler/"
-                            "issues/new attached with below information:\n\n"
+            raise TypeError("找不到正确的下载URL "
+                            "请到 "
+                            "https://github.com/xuanhun/tumblr-crawler"
+                            "提交错误信息:\n\n"
                             "%s" % post)
 
     def _download(self, medium_type, medium_url, target_folder):
@@ -118,37 +124,35 @@ class CrawlerScheduler(object):
         self.proxies = proxies
         self.queue = Queue.Queue()
         self.scheduling()
+      
 
     def scheduling(self):
-        # create workers
+        # 创建工作线程
         for x in range(THREADS):
             worker = DownloadWorker(self.queue,
                                     proxies=self.proxies)
-            # Setting daemon to True will let the main thread exit
-            # even though the workers are blocking
+            #设置daemon属性，保证主线程在任何情况下可以退出
             worker.daemon = True
             worker.start()
 
         for site in self.sites:
-            self.download_media(site)
-
-    def download_media(self, site):
-        self.download_photos(site)
-        self.download_videos(site)
+            if ISDOWNLOADIMG:
+                self.download_photos(site)
+            if ISDOWNLOADVIDEO:
+                self.download_videos(site)
+        
 
     def download_videos(self, site):
         self._download_media(site, "video", START)
-        # wait for the queue to finish processing all the tasks from one
-        # single site
+        # 等待queue处理完一个用户的所有请求任务项
         self.queue.join()
-        print("Finish Downloading All the videos from %s" % site)
+        print("视频下载完成 %s" % site)
 
     def download_photos(self, site):
         self._download_media(site, "photo", START)
-        # wait for the queue to finish processing all the tasks from one
-        # single site
+         # 等待queue处理完一个用户的所有请求任务项
         self.queue.join()
-        print("Finish Downloading All the photos from %s" % site)
+        print("图片下载完成 %s" % site)
 
     def _download_media(self, site, medium_type, start):
         current_folder = os.getcwd()
@@ -175,13 +179,6 @@ class CrawlerScheduler(object):
 
 
 def usage():
-    print("1. Please create file sites.txt under this same directory.\n"
-          "2. In sites.txt, you can specify tumblr sites separated only by "
-          "comma without any blank spaces.\n"
-          "3. Save the file and retry.\n\n"
-          "Sample File Content:\nsite1,site2\n\n"
-          "Or use command line options:\n\n"
-          "Sample:\npython tumblr-photo-video-ripper.py site1,site2\n\n\n")
     print(u"未找到sites.txt文件，请创建.\n"
           u"请在文件中指定Tumblr站点名，并以逗号分割，不要有空格.\n"
           u"保存文件并重试.\n\n"
@@ -191,9 +188,6 @@ def usage():
 
 
 def illegal_json():
-    print("Illegal JSON format in file 'proxies.json'.\n"
-          "Please refer to 'proxies_sample1.json' and 'proxies_sample2.json'.\n"
-          "And go to http://jsonlint.com/ for validation.\n\n\n")
     print(u"文件proxies.json格式非法.\n"
           u"请参照示例文件'proxies_sample1.json'和'proxies_sample2.json'.\n"
           u"然后去 http://jsonlint.com/ 进行验证.")
@@ -214,7 +208,7 @@ if __name__ == "__main__":
                 sys.exit(1)
 
     if len(sys.argv) < 2:
-        # check the sites file
+        #校验sites配置文件
         filename = "sites.txt"
         if os.path.exists(filename):
             with open(filename, "r") as f:
